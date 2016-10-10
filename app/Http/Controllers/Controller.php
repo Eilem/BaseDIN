398b9ec5d45ede641c305f29f182ff671fe4f101
eilem <eeilemm@gmail.com>
2016-10-10 19:18:02 -0300

    removendo métodos depreciados do controller, inserindo metatags no controller de erro

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,60 +6,9 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Meta;
-
-use App\Repositories\SettingsRepository;
 
 abstract class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
-
-    /**
-     * Seta as metatag da Home, definidas em settings
-     */
-    protected function homeMeta()
-    {
-        $settingsRepository = new SettingsRepository();
-        $settings = $settingsRepository->get();
-
-        Meta::meta('robots', 'index,follow');
-        Meta::meta( 'title', $settings->home_title );
-        Meta::meta('description', $settings->home_description);
-        Meta::meta( 'image', asset( getenv( 'DEFAULT_IMAGE') ) );
-        
-    }
-
-    /**
-     * Seta metatag
-     * @param String $title         Título da página
-     * @param String $description   Descrição da página
-     * @param String $image         Imagem da página
-     */
-    protected function meta($title, $description = null, $image = null)
-    {
-        $settingsRepository = new SettingsRepository();
-        $settings = $settingsRepository->get();
-
-        Meta::meta('robots', 'index,follow');
-        Meta::meta( 'title', $title . ' - ' . $settings->title );
-
-        if( $description )
-        {
-            Meta::meta('description', $description);
-
-        } else {
-
-            Meta::meta('description', $settings->description);
-        }
-
-        if ( $image )
-        {
-            Meta::meta( 'image', $image );
-
-        } else {
-            // exibe a imagem default na metatag
-            Meta::meta( 'image', asset( getenv( 'DEFAULT_IMAGE') ) );
-        }
-    }
 
 }
--- app/Http/Controllers/ErrorController.php
+++ app/Http/Controllers/ErrorController.php
@@ -3,22 +3,23 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Repositories\Traits\InfoPageTrait;
 
 class ErrorController extends Controller
 {
-
+    use InfoPageTrait;
 
     public function index()
     {
+        $this->titlePage = "Página não localizada";
+        $page = $this->checkAndSetInfoPage("/404");
 
-        $this->meta('Página não localizada');
-        $menuSelected = "";
-
-        $data = compact(
-                  'menuSelected'
-        );
-
-        return view('errors.404')->with($data);
+        if(!$page)
+        {
+            $this->setMetatag( $this->titlePage);
+        }
+      
+        return view('errors.404');
     }
 
 }
